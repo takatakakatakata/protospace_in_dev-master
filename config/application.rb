562aac572461895af2bedc69d3991e8143b965b2
takahirohayasaka <tkhr8964@gmail.com>
2018-09-06 11:57:46 +0900

    My first commit

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module Miuraspace
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
