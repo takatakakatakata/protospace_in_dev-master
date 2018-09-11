fa4ce04e9cb6c5503b74b9a479ae4006b3e97ba4
yodako <yoda@sana.local>
2018-09-11 10:40:12 +0900

    tag_view 作成

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :captured_images, dependent: :destroy
   has_many :tags, through: :prototype_tag
-  has_many :prototype_tags
+  has_many :prototype_tags, dependent: :destroy
 
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
   has_many :prototypes, through: :prototype_tag
-  has_many :prototype_tags
+  has_many :prototype_tags, dependent: :destroy
 end
